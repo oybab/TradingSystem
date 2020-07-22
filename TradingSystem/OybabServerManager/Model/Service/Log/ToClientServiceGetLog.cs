43f72bc200916ddf11eea6be5f77d1d26529ebd2
Oybab <mmtj@oybab.net>
2020-07-23 02:01:29 +0800

    add Missing files.

---
+++ TradingSystem/OybabServerManager/Model/Service/Log/ToClientServiceGetLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Oybab.ServerManager.Model.Service.Log
+{
+
+    public class ToClientServiceGetLog : ToClientService
+    {
+
+        public bool Result { get; set; }
+
+        public string Logs { get; set; }
+
+        public string Balance { get; set; }
+    }
+}
---
+++ TradingSystem/OybabServerManager/Model/Service/Log/ToServerServiceGetLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Oybab.ServerManager.Model.Service.Log
+{
+
+    public class ToServerServiceGetLog : ToServerService
+    {
+
+        public long AdminId { get; set; }
+
+        public long AddTimeStart { get; set; }
+
+        public long AddTimeEnd{ get; set; }
+
+        public long IsBalanceChange { get; set; }
+
+        public long BalanceType { get; set; }
+
+        public long OperateId { get; set; }
+
+        public long IsBalancePrice { get; set; }
+
+        public long OperateTypeId { get; set; }
+
+    }
+}
